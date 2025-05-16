ffb01655568920ea3ac84655ea898280aad09209
Danniel <danniel@ui.ac.id>
2025-05-16 17:19:16 +0700

    Delete subscriber directory

--- subscriber/target/debug/subscriber.d
+++
@@ -1,1 +0,0 @@
-/Users/dannielang/Documents/Semester\ 4/Adpro/tutorial-08-subscriber/subscriber/target/debug/subscriber: /Users/dannielang/Documents/Semester\ 4/Adpro/tutorial-08-subscriber/subscriber/src/main.rs
